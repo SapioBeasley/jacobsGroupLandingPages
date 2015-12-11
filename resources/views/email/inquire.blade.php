0ce8b8d3114382136ba1ebb350ccfc9c3d75f4f9
Andreas Beasley <andreas@sapioweb.com>
2015-12-11 04:34:20 -0800

    email view added

---
+++ resources/views/email/inquire.blade.php
@@ -0,0 +1,11 @@
+<div align="center" style="background-color:#ededed;padding-top: 25px;">
+	<img src="http://jacobsgroupvegas.com/wp-content/uploads/2014/06/final-logo-long.png" width="240px" height="auto">
+</div>
+<h1>You have a new Lead</h1>
+<h3>{{$data['titleStrong']}} {{$data['title']}}</h3>
+<br><hr><br>
+<h3>Lead</h3>
+<strong>Name:</strong> {{$data['first_name']}} <br>
+<strong>Email:</strong> {{$data['email']}} <br>
+<strong>Phone:</strong> {{$data['phone']}} <br><br>
+http://jacobsgroupvegas.com/{{$data['slug']}} <br>
---
+++ resources/views/sendSuccess.blade.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>Thank You For your Inquiry</title>
+		<meta http-equiv="refresh" content="8;url=http://jacobsgroupvegas.com/">
+		<link href="{{asset('css/style.css')}}" rel="stylesheet">
+		<link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
+	</head>
+	<body>
+	<div style="width:470px;margin:200px auto;">
+		<div id="quote_holder">
+			<div class="flexslider">
+				<ul class="slides">
+					<!--Testimonial #1 -->
+					<li>
+						<div class="testimonials">
+							<blockquote class="quote-text" style="text-align:center;">
+								Thank you for your inquiry, someone from our team will be in touch shortly.<br /><br /> In the mean time you may also give us a call. (702)442-0055
+							</blockquote>
+						</div>
+					</li>
+				</ul>
+			</div>   <!-- End Rotator Content -->
+		</div>  <!-- End Quote -->
+	</div>
+	</body>
+</html>
