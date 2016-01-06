bb9d9cf85c3f4fea3e7277f57271f4723b78139e
Andreas Beasley <andreas@sapioweb.com>
2016-01-05 21:15:39 -0800

    redirect to path

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 class AuthController extends Controller
 {
 
-    protected $redirectPath = '/';
+    protected $redirectTo = '/';
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,25 +86,25 @@
     <script src="{{asset('js/dropzone.js')}}"></script>
 
     @if (isset($program))
-    <script type="text/javascript">
-        var baseUrl = "{{ route('program', $program['id']) }}";
-        var token = "{{ Session::getToken() }}";
-        Dropzone.autoDiscover = false;
-        var myDropzone = new Dropzone("div#dropzoneFileUpload", {
-            url: baseUrl + "/upload",
-            params: {
-                _token: token
-            }
-        });
-        Dropzone.options.myAwesomeDropzone = {
-            paramName: "file", // The name that will be used to transfer the file
-            maxFilesize: 200, // MB
-            addRemoveLinks: true,
-            accept: function(file, done) {
+        <script type="text/javascript">
+            var baseUrl = "{{ route('program', $program['id']) }}";
+            var token = "{{ Session::getToken() }}";
+            Dropzone.autoDiscover = false;
+            var myDropzone = new Dropzone("div#dropzoneFileUpload", {
+                url: baseUrl + "/upload",
+                params: {
+                    _token: token
+                }
+            });
+            Dropzone.options.myAwesomeDropzone = {
+                paramName: "file", // The name that will be used to transfer the file
+                maxFilesize: 200, // MB
+                addRemoveLinks: true,
+                accept: function(file, done) {
 
-            },
-        };
-    </script>
+                },
+            };
+        </script>
     @endif
 </body>
 </html>
