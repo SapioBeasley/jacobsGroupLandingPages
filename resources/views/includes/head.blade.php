715260f3e35d76c1349f7f77c20be64c5a3152a1
Andreas Beasley <andreas@sapioweb.com>
2016-01-29 17:02:43 -0800

    fb pixel added

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -31,6 +31,20 @@
 <!-- Google Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Lato:400,900italic,900,700italic,400italic,300italic,300,100italic,100' rel='stylesheet' type='text/css'>
 
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1988087694750080');
+fbq('track', "PageView");
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1988087694750080&ev=PageView&noscript=1"/></noscript>
+<!-- End Facebook Pixel Code -->
+
 <style type="text/css">
 	#intro {
 		background-image: url({{asset('uploads/' . $program['slug'] . '.jpg')}});
