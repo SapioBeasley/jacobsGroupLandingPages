722365f4a91206ccfb4e36222977f2e1f207586a
Andreas Beasley <andreas@sapioweb.com>
2016-02-04 11:00:38 -0800

    date added to csv file

--- app/Http/Controllers/DownloadFeedController.php
+++ app/Http/Controllers/DownloadFeedController.php
@@ -13,7 +13,7 @@ class DownloadFeedController extends Controller
 {
     public function downloadFeedCsv()
     {
-        $file = '../storage/feeds/ad-feed.csv';
+        $file = '../storage/feeds/ad-feed-' . date('m-d-Y') . '.csv';
 
         $programs = CrudHelper::index(new \App\Program)->get();
 
@@ -52,81 +52,5 @@ class DownloadFeedController extends Controller
         fclose($csvFile);
 
         return response()->download($file);
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
