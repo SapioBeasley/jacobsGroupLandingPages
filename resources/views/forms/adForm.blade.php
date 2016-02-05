41caa09cfeae6134ddb9c2019501b5f26b8f4b72
Andreas Beasley <andreas@sapioweb.com>
2016-02-04 16:13:13 -0800

    adform added

---
+++ resources/views/forms/adForm.blade.php
@@ -0,0 +1,28 @@
+<div class="col-md-6">
+	<p><strong>ID </strong><br>
+	{{$program['slug']}}
+	<p><strong>Item Title </strong><br>
+	{{$program['titleStrong']}}
+	<p><strong>Destination URL </strong><br>
+	{{url('/', $program['slug'])}}
+	<p><strong>Item subtitle </strong><br>
+	{{$program['title']}}
+	<p><strong>Item description </strong><br>
+	{{$program['bullet2']}}
+	<p><strong>Item category </strong><br>
+	real estate
+	<p><strong>Contextual keywords </strong><br>
+	closing costs;home assistance;buying home;real estate;
+</div>
+
+<div class="col-md-6">
+
+	@if (isset($program))
+		<div class="form-group">
+			<div class="dropzone" id="dropzoneFileUpload"></div>
+		</div>
+	@endif
+
+	<p><strong>Image URL </strong><br>
+	{{url('/', $program['slug']) . '/uploads/ads/' . $program['slug']. '.jpg'}}
+</div>
