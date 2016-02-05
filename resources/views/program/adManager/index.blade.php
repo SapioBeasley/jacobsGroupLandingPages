9165af8bce3d30b926b72d00188506a1c05f9390
Andreas Beasley <andreas@sapioweb.com>
2016-02-04 16:18:14 -0800

    new ad manager page

---
+++ resources/views/program/adManager/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container spark-screen">
+		<div class="row">
+			<div class="col-md-12">
+
+				@include('includes.status')
+
+				<div class="panel panel-default">
+					<div class="panel-heading">Program Ad Manager</div>
+
+					<div class="panel-body">
+						{!! Form::model($program, ['route' => ['program.ad.manager.update', $program['slug']], 'method' => 'PUT']) !!}
+
+							@include('forms.adForm')
+
+							<div class="col-md-12">
+								<div class="form-group">
+									{!! Form::submit('Update Ad Details', ['class' => 'btn btn-primary']) !!}
+								</div>
+							</div>
+
+						{!! Form::close() !!}
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+
+@section('uploadScript')
+	@if (isset($program))
+		<script type="text/javascript">
+			var baseUrl = "{{ route('program', $program['slug']) }}";
+			var token = "{{ Session::getToken() }}";
+			Dropzone.autoDiscover = false;
+			var myDropzone = new Dropzone("div#dropzoneFileUpload", {
+				url: baseUrl + "/upload-ad",
+				params: {
+					_token: token
+				}
+			});
+			Dropzone.options.myAwesomeDropzone = {
+				paramName: "file", // The name that will be used to transfer the file
+				maxFilesize: 200, // MB
+				addRemoveLinks: true,
+				accept: function(file, done) {
+
+				},
+			};
+		</script>
+	@endif
+@endsection
